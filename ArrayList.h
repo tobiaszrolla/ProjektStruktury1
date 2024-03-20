27f5ac6fcee60850e5f3604bc3b13639359d332c
Tobiasz Rolla <rollatobiasz@gmail.com>
2024-03-20 21:19:24 +0100

    20.03.2024 21:18 Wykonano ArrayList -dodawanie usuwanie front -dodawanie usuwanie tył -dodawanie usuwanie dowolne miejsce

--- ArrayList.h
+++ ArrayList.h
@@ -11,20 +11,23 @@ private:
     unsigned size;
     unsigned capacity;
     T *array;
-    void Resaiz();
-    void HelpAddFront();
+    void Resaiz(unsigned newCapacity);
 public:
     ArrayList();
     ~ArrayList();
     unsigned Size();
-    T& GetPlace(unsigned index);
+    unsigned Capacity();
     T GetValue(unsigned index);
     void AddBack(T number);
     void AddFront(T number);
+    void AddPlace(T number, unsigned index);
+    void DelateBack();
+    void DelateFront();
+    void DelatePlace(unsigned index);
 };
 //Podstawowy Konstruktor z ustalonym rozmiarem
 template <typename T>
-ArrayList<T>::ArrayList(): size{0}, capacity{5}{
+ArrayList<T>::ArrayList(): size{0}, capacity{1}{
     array = new T[capacity];
 }
 //Destruktor
@@ -34,41 +37,34 @@ ArrayList<T>::~ArrayList(){
 }
 //Metoda powiększająca ArrayList w momęcie przekroczenia rozmiaru
 template <typename T>
-void ArrayList<T>::Resaiz(){
-    T *array_pom = new T[capacity*2];
-    for(int i = 0; i < capacity; ++i){
+void ArrayList<T>::Resaiz(unsigned newCapacity){
+    capacity = newCapacity;
+    T *array_pom = new T[capacity];
+    for(int i = 0; i < size; i++){
         array_pom[i] = array[i];
     }
     delete[] array;
     array = array_pom;
-    capacity = capacity*2;
     
 }
-template <typename T>
-void ArrayList<T>::HelpAddFront(){
-        T *array_pom = new T[capacity];
-        for(int i = 0; i < capacity; ++i){
-        array_pom[i+1] = array[i];
-        }
-        delete[] array;
-        array = array_pom;
-}
+
 template <typename T>
 unsigned ArrayList<T>::Size(){
     return size;
 }
 template <typename T>
-T& ArrayList<T>::GetPlace(unsigned index){
-    if(index >= 0 && index < size){
-        return &array[index];
-    }
+unsigned ArrayList<T>::Capacity(){
+    return capacity;
 }
 
 //metoda zwracjaąca wartość na danym indexie
 template <typename T>
 T ArrayList<T>::GetValue(unsigned index){
-    if(index >= 0 && index < size){
+    if(index < size- 1){
         return array[index];
+    }
+    else{
+        cout<<"out of range"<<endl;
     }
 }
 
@@ -80,21 +76,86 @@ void ArrayList<T>::AddBack(T number){
         size++;
     }
     else{
-        Resaiz();
+        Resaiz(capacity*2);
         array[size] = number;
         size++;
     }
 }
+
+//metoda dodająca na początek
 template <typename T>
 void ArrayList<T>::AddFront(T number){
-    if(size < capacity){
+    if(size == capacity){
+        capacity *= 2;
+        T *array_pom = new T[capacity];
+        for(int i = 0; i < size; i++){
+        array_pom[i+1] = array[i];
+        }
+        delete[] array;
+        array = array_pom;
     }
     else{
+        for(int i = size; i>0; i--){
+            array[i] = array[i-1];
+        }
+    }
+    array[0] = number;
+    size++;
+}
+
+//metoda usuwająca z tyłu
+template <typename T>
+void ArrayList<T>::DelateBack(){
+    if(capacity > 0){
+        size--;
+        Resaiz(size);
     }
 }
 
-
-
-
-
+//metoda usuwająca pierwszy element
+template <typename T>
+void ArrayList<T>::DelateFront(){
+    if(capacity > 0){
+        size--;
+        T *array_pom = new T[size];
+        for(int i = 0; i < size; i++){
+            array_pom[i] = array[i+ 1];
+        }
+        delete[] array;
+        array = array_pom;
+        capacity = size;
+    }
+}
+//Dodaje element na dowolne miejsce;
+template <typename T>
+void ArrayList<T>::AddPlace(T number, unsigned index){
+    if(index < capacity){
+        array[index] = number;
+    }
+    else{
+        Resaiz(index+1);
+        for(int i = size; i<index + 1; i++){
+            array[i] = 0;
+        }
+        array[index] = number;
+        size = index + 1;
+    }
+}
+//Usuwa dowolny element
+template <typename T>
+void ArrayList<T>::DelatePlace(unsigned index){
+    if(index <= capacity){
+        T *array_pom =new T[size - 1];
+        for(int i = 0; i < index; i++){
+            array_pom[i] = array[i];
+        }
+        for(int i = index; i < size; i++){
+            array_pom[i] = array[i + 1];
+        }
+        delete[] array;
+        array = array_pom;
+        size = size--;
+        capacity = size;
+    }
+}
 #endif
--- main.cpp
+++ main.cpp
@@ -8,11 +8,25 @@ using namespace std;
 int main(){
     //Program na testy
     ArrayList<int> Array;
-    int z = 0;
-    while(z<5){
-        Array.AddFront(z*2);
-        cout<<Array.GetValue(z)<<endl;
-        z++;
+    for(int i = 1; i <= 20; i++){
+        Array.AddFront(i*3);
     }
+    for(int i = 0; i < 20; i++){
+        cout<<Array.GetValue(i)<<endl;
+    }
+    cout<<Array.Size()<<endl;
+    cout<<Array.Capacity()<<endl<<endl;
+    /*for(int i = 0; i <5; i++){
+        Array.DelateFront();
+    }*/
+    //Array.AddPlace(666,19);
+    Array.DelatePlace(19);
+    cout<<Array.Size()<<endl;
+    cout<<Array.Capacity()<<endl<<endl;
+    for(int i = 0; i < 20; i++){
+        cout<<Array.GetValue(i)<<endl;
+    }
+    cout<<Array.GetValue(20)<<endl;
+    
     return 0;
 }
